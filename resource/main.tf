5cd773bf8214c8c55c28e69a413e94157b82311f
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-20 14:49:36 +0530

    Roboshop Project

--- resource/main.tf
+++ resource/main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "example" {
 }
 
 data "azurerm_subnet" "example" {
-  name                 = "default"
+  name                 = "Default"
   virtual_network_name = "project-setup-network"
   resource_group_name  = "data.azurerm_resource_group.example.name"
 }
