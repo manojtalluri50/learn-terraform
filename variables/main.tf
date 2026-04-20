213afac4688414520d82877ce8707c2bb169fcb4
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-20 17:21:45 +0530

    Roboshop Project

---
+++ variables/main.tf
@@ -0,0 +1,40 @@
+variable "s1" {
+  default = "Hello"
+}
+
+variable "n1" {
+  default = 2
+}
+
+variable "b1" {
+  default = true
+}
+
+
+# Data types
+
+variable "v1" {
+  default = "Hello"
+}
+
+variable "v2" {
+  default = ["Hello",2,true]
+}
+variable "v3" {
+  default = {
+    course="devops"
+    cloud="azure"
+  }
+}
+
+output "o1" {
+  value = var.v1
+}
+
+output "o2" {
+  value = "$(var.v1) - Jhon"
+}
+
+output "o3" {
+  value = ""
+}
