5c08f43534c133ea9c286b7346af863daa7b091f
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-21 14:21:49 +0530

    Roboshop Project

--- loops-count/main.tf
+++ loops-count/main.tf
@@ -1,3 +1,14 @@
+# resource "null_resource" "demo" {
+#   count = 10
+# }
+
+
 resource "null_resource" "demo" {
-  count = 10
+   count =length(var.demo)
+}
+
+variable "demo" {
+  default = [
+  "apple",
+  "banana"]
 }
