28feb9603d88e46adea037241eab5e166b6717f8
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-21 18:29:35 +0530

    Roboshop Project

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -11,57 +11,57 @@ variable "components" {
 
     frontend={
       name="frontend"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     mongodb={
       name="mongodb"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     catalogue={
       name="catalogue"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     redis={
       name="redis"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     user={
       name="user"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     cart={
       name="cart"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     mysql={
       name="mysql"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     shipping={
       name="shipping"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     rabbitmq={
       name="rabbitmq"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     payment={
       name="payment"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
     dispatch={
       name="dispatch"
-      vm_size= "Standard_D2s_v3"
+      vm_size= "Standard_D2s_v2"
     }
 
   }
