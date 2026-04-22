2be030ccc543a87da1f214b12b0848908e7773ea
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-22 16:20:25 +0530

    Roboshop Project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -89,3 +89,16 @@ resource "azurerm_virtual_machine" "main" {
     environment = var.component
   }
 }
+
+resource "azurerm_dns_zone" "main" {
+  name                = "azdevopsb82.online"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_dns_a_record" "main" {
+  name                = "${var.component}-dev"
+  zone_name           = azurerm_dns_a_record.main.name
+  resource_group_name = data.azurerm_resource_group.example.name
+  ttl                 = 10
+  records             = [azurerm_network_interface.main.private_ip_address]
+}
