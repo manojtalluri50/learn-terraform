7e02dd3328e305352095a43cfab2f54af99991e3
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-22 15:02:19 +0530

    Roboshop Project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -11,6 +11,33 @@ resource "azurerm_network_interface" "main" {
 
   }
 }
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.component}-nsg"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "main"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.component
+  }
+}
 resource "azurerm_public_ip" "main" {
   name                = "${var.component}-ip"
   resource_group_name = data.azurerm_resource_group.example.name
