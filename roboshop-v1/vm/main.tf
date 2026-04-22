6b0f2befe3e1c0d9f53379f045d1a48316a8dfa1
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-22 18:13:35 +0530

    Roboshop Project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -109,7 +109,6 @@ resource "null_resource" "ansible" {
 
     }
     inline = [
-      "sleep 60",
       "sudo dnf update -y",
       "sudo dnf install python3 python3-pip git -y",
       "sudo python3 -m pip install ansible",
