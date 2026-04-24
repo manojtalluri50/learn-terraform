491d5d5aa2e3f7f80e98568a21cc3c747016de91
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-24 18:18:58 +0530

    Roboshop Project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_virtual_machine" "main" {
     publisher = "RedHat"
     offer     = "RHEL"
     sku       = "9-lvm-gen2"
-    version   = "9.4.2024030100"
+    version   = "latest"
   }
 
   storage_os_disk {
