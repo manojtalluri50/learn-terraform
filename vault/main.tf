935162291786194026ed26cbf59005b2aa7c0aec
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-04 14:43:47 +0530

    Roboshop Project

--- vault/main.tf
+++ vault/main.tf
@@ -17,6 +17,12 @@ data "vault_kv_secret" "secret_data" {
 
 resource "local_file" "local" {
   filename = "/tmp/pass"
+  content = data.vault_kv_secret.secret_data.data_json["password"]
+
+}
+
+resource "local_file" "full" {
+  filename = "/tmp/pass"
   #content = data.vault_kv_secret.secret_data.data
   content=replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
 }
