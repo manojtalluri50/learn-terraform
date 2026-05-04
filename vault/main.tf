9d86a6a919df12ad19abcae5ada73633c914f89e
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-04 14:39:10 +0530

    Roboshop Project

--- vault/main.tf
+++ vault/main.tf
@@ -18,5 +18,5 @@ data "vault_kv_secret" "secret_data" {
 resource "local_file" "local" {
   filename = "/tmp/pass"
   #content = data.vault_kv_secret.secret_data.data
-  content=replace(replace(jsonencode(data.vault_kv_secret.secret_data.data.tags), "\"", ""), ":", "=")
+  content=replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
 }
